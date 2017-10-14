cff83638ce657988735607acb52b8d54a9ac807e
monicairiart <monicairiartcordoba@gmail.com>
2017-10-14 10:23:27 -0300

    pruebas de datos de modulo - refactorizado

--- Obligatorio1DA/PruebasUnitarias/PruebasDocente.cs
+++ Obligatorio1DA/PruebasUnitarias/PruebasDocente.cs
@@ -8,18 +8,22 @@ namespace PruebasUnitarias
     [TestClass]
     public class PruebasDocente
     {
+        public MantenimientoDocente mantenimientoDocente { get; set; }
+
+        
+        public PruebasDocente()
+        {
+            mantenimientoDocente = new MantenimientoDocente();
+        }
+
         [TestMethod]
         public void ProbarTipoModuloGestionDocente()
         {
-            // Creamos el ambDocente para gestionar los docentes
-            MantenimientoDocente mantenimientoDocente = new MantenimientoDocente();
             Assert.IsInstanceOfType(mantenimientoDocente, typeof(IModuloGestion));
         }
         [TestMethod]
         public void ProbarDatosTipoModuloGestionDocente()
         {
-            // Test si abm tiene nombre y descripción
-            MantenimientoDocente mantenimientoDocente = new MantenimientoDocente();
             Assert.AreEqual("Gestion Docente", mantenimientoDocente.Nombre);
             Assert.AreEqual("Alta, Baja y Modificación de Docentes", mantenimientoDocente.Descripcion);
         }
