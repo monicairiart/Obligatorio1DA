53c1dd8b0ad2e55ed43d77ac4f1173a11ff380b3
monicairiart <monicairiartcordoba@gmail.com>
2017-10-16 01:48:27 -0300

    prueba tipo modulo estado verde

--- Obligatorio1DA/PruebasUnitarias/PruebasAlumno.cs
+++ Obligatorio1DA/PruebasUnitarias/PruebasAlumno.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GestionAlumno;
+using Interfaces;
 
 namespace PruebasUnitarias
 {
     [TestClass]
     public class PruebasAlumno
     {
+        public MantenimientoAlumno mantenimientoAlumno { get; set; }
         [TestMethod]
         public void ProbarTipoModuloGestionAlumno()
         {
