b973657b302f8c8ce64cd77744ddc3c09b9ac254
monicairiart <monicairiartcordoba@gmail.com>
2017-10-16 03:22:02 -0300

    Prueba de modificar el nombre de alumno - estado rojo

--- Obligatorio1DA/PruebasUnitarias/PruebasAlumno.cs
+++ Obligatorio1DA/PruebasUnitarias/PruebasAlumno.cs
@@ -95,11 +95,31 @@ namespace PruebasUnitarias
             List<Alumno> misAlumnos = new List<Alumno>();
             misAlumnos.Add(mantenimientoAlumno.AltaDatosAlumno("Juana", "Sosa", "1000", new List<string>()));
             misAlumnos.Add(mantenimientoAlumno.AltaDatosAlumno("Paola", "Bianco", "1001", new List<string>()));
-            misAlunnos.Add(mantenimientoAlumno.AltaDatosAlumno("Hugo", "Cabral", "1002", new List<string>()));
+            misAlumnos.Add(mantenimientoAlumno.AltaDatosAlumno("Hugo", "Cabral", "1002", new List<string>()));
             misAlumnos.Add(mantenimientoAlumno.AltaDatosAlumno("Alejandra", "Suarez", "1003", new List<string>()));
             CollectionAssert.AreEqual(misAlumnos, mantenimientoAlumno.GetAlumnos());
             mantenimientoAlumno.BajaAlumno("123466");
             CollectionAssert.AreEqual(misAlumnos, mantenimientoAlumno.GetAlumnos());
+        }
+        [TestMethod]
+        public void ProbarModificacionAlumnoNombre()
+        {
+            // Creamos una lista de docentes para realizar las validaciones
+            List<Alumno> misAlumnos = new List<Alumno>();
+
+            // Agrego Alumnos con AltaDatosAlumno para tener una lista
+            misAlumnos.Add(mantenimientoAlumno.AltaDatosAlumno("Juana", "Sosa", "1000", new List<string>()));
+            misAlumnos.Add(mantenimientoAlumno.AltaDatosAlumno("Paola", "Bianco", "1001", new List<string>()));
+            misAlumnos.Add(mantenimientoAlumno.AltaDatosAlumno("Hugo", "Cabral", "1002", new List<string>()));
+            misAlumnos.Add(mantenimientoAlumno.AltaDatosAlumno("Alejandra", "Suarez", "1003", new List<string>()));
+
+   
+            // Creamos un nuevo alumno para modificar
+            Alumno nuevosValoresAlumno = new Alumno();
+            nuevosValoresAlumno.Nombre = "Juanita";
+            mantenimientoAlumno.ModificarAlumno("1000", nuevosValoresAlumno);
+            Assert.AreEqual("Juanita", misAlumnos[0].Nombre);
+            Console.WriteLine("nvos valor nombre " + misAlumnos[0].Nombre);
         }
     }
 }
