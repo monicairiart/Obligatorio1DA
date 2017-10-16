f658fa606c460eb6f9119f68467fa23e7348d176
monicairiart <monicairiartcordoba@gmail.com>
2017-10-16 01:33:56 -0300

    Prueba tipo modulo estado rojo

---
+++ Obligatorio1DA/PruebasUnitarias/PruebasAlumno.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PruebasUnitarias
+{
+    [TestClass]
+    public class PruebasAlumno
+    {
+        [TestMethod]
+        public void ProbarTipoModuloGestionAlumno()
+        {
+            Assert.IsInstanceOfType(mantenimientoAlumno, typeof(IModuloGestion));
+        }
+    }
+}
