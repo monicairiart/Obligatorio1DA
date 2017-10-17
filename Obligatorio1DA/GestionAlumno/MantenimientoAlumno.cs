ade0ac66dd4fe4e4494a1580aa3ebc9524fc1d42
monicairiart <monicairiartcordoba@gmail.com>
2017-10-17 16:44:34 -0300

    problema 171017

---
+++ Obligatorio1DA/GestionAlumno/MantenimientoAlumno.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Interfaces;
+using System.Collections;
+
+namespace GestionAlumno
+{
+    public class MantenimientoAlumno : IModuloGestion
+    {
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public object Menu { get; set; }
+        public IList Acciones { get; set; }
+
+        private List<Alumno> alumnos = new List<Alumno>();
+        public List<Alumno> GetAlumnos()
+        {
+            return alumnos;
+        }
+        public MantenimientoAlumno()
+        {
+            Console.WriteLine();
+            Nombre = "Gestion Alumno";
+            Descripcion = "Alta, Baja y Modificación de Alumnos";
+        }
+        public Alumno AltaDatosAlumno(string nombreAlumno, string apellidoAlumno, string ciAlumno, List<string> materias)
+        {
+            Alumno alumno = new Alumno();
+            alumno.Nombre = nombreAlumno;
+            alumno.Apellido = apellidoAlumno;
+            alumno.Ci = ciAlumno;
+            alumno.Materias = materias;
+            alumnos.Add(alumno);
+            return alumno;
+        }
+        public void BajaAlumno(string ci)
+        {
+            Console.WriteLine("Cedula de Identidad a Baja de Alumno > " + ci);
+
+            try
+            {
+                Alumno alumnoAEliminar = alumnos.Single(alumno => alumno.Ci == ci);
+                alumnos.Remove(alumnoAEliminar);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Excepcion al filtrar alumno > " + e.ToString());
+            }
+        }
+        public void ModificarAlumno(string ci, Alumno nuevosValores)
+        {
+            Console.WriteLine("Alumno a modificar > " + ci);
+            try
+            {
+                Alumno alumnoAModificar = alumnos.Single(alumno => alumno.Ci == ci);
+                int indiceDelAlumnoAModificar = alumnos.IndexOf(alumnoAModificar);
+
+                alumnos[indiceDelAlumnoAModificar].Nombre = nuevosValores.Nombre != "" ? nuevosValores.Nombre : alumnoAModificar.Nombre;
+
+                alumnos[indiceDelAlumnoAModificar].Apellido = nuevosValores.Apellido != "" ? nuevosValores.Apellido : alumnoAModificar.Apellido;
+                alumnos[indiceDelAlumnoAModificar].Ci = nuevosValores.Ci != "" ? nuevosValores.Ci : alumnoAModificar.Ci;
+
+                alumnos[indiceDelAlumnoAModificar].Materias = nuevosValores.Materias[0] != "" ? nuevosValores.Materias : alumnoAModificar.Materias;
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Excepcion al filtrar alumno > " + e.ToString());
+            }
+        }
+    }
+}
---
+++ Obligatorio1DA/PruebasUnitarias/PruebasMateria.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PruebasUnitarias
+{
+    [TestClass]
+    public class PruebasMateria
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+        [TestMethod]
+        public void ProbarTipoModuloGestionMateria()
+        {
+            Assert.IsInstanceOfType(mantenimientoMateria, typeof(IModuloGestion));
+        }
+    }
+}
