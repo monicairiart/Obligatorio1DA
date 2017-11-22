7878cd09f96a2d78078d883b6994525ada2922f0
monicairiart <monicairiartcordoba@gmail.com>
2017-11-22 09:22:54 -0300

    persistencia- alta docente - estado rojo

--- Obligatorio1DA/Persistencia/ContextoDb.cs
+++ Obligatorio1DA/Persistencia/ContextoDb.cs
@@ -1,12 +1,19 @@
-﻿using System;
+﻿using GestionAlumno;
+using GestionDocente;
+using GestionMateria;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Persistencia
 {
-    public class Class1
+    public class ContextoDb:DbContext
     {
+        public DbSet<Alumno> Alumnos { get; set; }
+        public DbSet<Docente> Docentes { get; set; }
+        public DbSet<Materia> Materias { get; set; }
     }
 }
---
+++ Obligatorio1DA/PruebasUnitarias/PruebasPersistencia.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Persistencia;
+using System.Data.Entity;
+using GestionDocente;
+using System.Linq;
+
+namespace PruebasUnitarias
+{
+    [TestClass]
+    public class PruebasPersistencia
+    {
+        public ContextoDb contextoDb { get; set; }
+        public PruebasPersistencia()
+        {
+            contextoDb = new ContextoDb();
+        }
+
+        [TestMethod]
+        public void ProbarContextoDb()
+        {
+            Assert.IsInstanceOfType(contextoDb, typeof(DbContext));
+        }
+        [TestMethod]
+        public void ProbarConexionBaseDatos()
+        {
+            Assert.IsTrue(contextoDb.Database.Exists());
+        }
+        [TestMethod]
+        public void ProbarAltaDocenteBaseDatos()
+        {
+            Docente nuevosValoresDocente = new Docente();
+            List<Docente> docenteDb = new List<Docente>();
+            nuevosValoresDocente.Ci = "18681749";
+            nuevosValoresDocente.Nombre = "Monica";
+            nuevosValoresDocente.Apellido = "Iriart";
+            contextoDb.Docentes.Add(nuevosValoresDocente);
+            contextoDb.SaveChanges();
+            docenteDb = contextoDb.Docentes.SqlQuery("Select * from Docentes where Ci = '18681749'").ToList();
+            Console.WriteLine("docentes db " + docenteDb[0].Nombre + docenteDb[0].Apellido + docenteDb[0].Ci);
+            Assert.AreEqual(docenteDb[0].Ci, nuevosValoresDocente.Ci);
+        }
+    }
+}
